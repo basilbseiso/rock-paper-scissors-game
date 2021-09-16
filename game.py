15992d4b2e9fb9a23317cade57c43d5ee3daff61
basilbseiso <bb1026@georgetown.edu>
2021-09-16 12:06:14 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import random
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 #prompt user for input
@@ -6,3 +9,10 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
 print(user_choice)
+
+# Computer choice (at random)
+
+options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(options)
+print(computer_choice)
