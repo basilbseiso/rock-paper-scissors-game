a8c0830c2f94a6b18371eb163363b93596ceb9f1
basilbseiso <bb1026@georgetown.edu>
2021-09-21 11:58:56 -0400

    Update game.py

--- game.py
+++ game.py
@@ -18,3 +18,22 @@ options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 print("Computer chose:")
 print(computer_choice)
+
+
+if(user_choice == "rock" and computer_choice == "scissors"):
+    print("Congratulations,you win!")
+elif(user_choice == "rock" and computer_choice == "paper"):
+    print("Oh, the computer won. It's ok.")
+elif(user_choice == "paper" and computer_choice == "scissors"):
+    print("Oh, the computer won. It's ok.")
+elif(user_choice == "paper" and computer_choice == "rock"):
+    print("Congratulations,you win!")
+elif(user_choice == "scissors" and computer_choice == "rock"):
+    print("Oh, the computer won. It's ok.")
+elif(user_choice == "scissors" and computer_choice == "paper"):
+    print("Congratulations,you win!")
+else: print("It's a tie!")
+
+
+
+print("THANKS PLEASE PLAY AGAIN")
